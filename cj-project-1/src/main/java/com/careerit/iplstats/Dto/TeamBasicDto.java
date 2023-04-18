80c4e796c37d1d08d2894f6f4efee55221a6c236
Yogichandar <boppanachandu1@gmail.com>
2023-04-18 15:08:56 -0400

    Added IplStats Module

---
+++ cj-project-1/src/main/java/com/careerit/iplstats/Dto/TeamBasicDto.java
@@ -0,0 +1,12 @@
+package com.careerit.iplstats.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TeamBasicDto
+{
+    private String team;
+    private String teamName;
+}
